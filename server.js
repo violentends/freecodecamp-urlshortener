8b2b07dc5c15aca9593649095e73ce059adbe7af
Timothy McClure <timothy.r.mcclure@gmail.com>
2020-10-24 12:24:13 -0500

    got it working

--- server.js
+++ server.js
@@ -1,37 +1,100 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var bodyParser = require("body-parser");
+var sqlite3 = require("sqlite3");
+var dbFile = "./db.sqlite3";
+// const knex = require("knex");
+// const db = knex({
+//   client: "sqlite3",
+//   connection: {
+//     filename: "./db.sqlite3",
+//   },
+//   debug: true,
+//   useNullAsDefault: true,
+// });
 
-var cors = require('cors');
-
+var cors = require("cors");
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.DB_URI);
+let db = new sqlite3.Database(dbFile);
+db.run(`
+  create table if not exists urls (
+    url text,
+    id integer primary key autoincrement
+  );
+`);
+db.close();
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.post("/api/shorturl/new", function (req, res) {
+  let url = "";
+  try {
+    let urlObj = new URL(req.body.url);
+    url = urlObj.toString();
+  } catch (error) {
+    res.json({
+      error: "invalid url",
+    });
+    return;
+  }
+  let db = new sqlite3.Database(dbFile);
+  db.run(
+    ` insert into urls (url)
+  values (?);`,
+    [url],
+    function (err) {
+      if (err) {
+        console.log("error: ", err);
+        res.json({ error: "db error" });
+      }
+    }
+  );
+  db.get(`select * from urls where url = ?`, [url], function (err, row) {
+    if (err) {
+      res.json({ error: "db error" });
+    }
+    res.json({
+      original_url: row.url,
+      short_url: row.id,
+    });
+  });
+  db.close();
+});
+
+app.get("/api/shorturl/:id", function (req, res) {
+  let id = parseInt(req.params.id);
+  let db = new sqlite3.Database(dbFile);
+  db.get("select * from urls where id = ? ", [id], function (err, row) {
+    if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      res.redirect(row.url);
+    }
+  });
+});
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...", port);
 });
